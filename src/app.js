30ef7f22d8da3054df5a7e0c49e803cabdbd709b
Giovanna <giovannad1clonius@gmail.com>
2023-04-05 20:44:01 -0300

    Posts adicionados

--- src/app.js
+++ src/app.js
@@ -1,6 +1,37 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+const usuarios = [];
+const tweets = [];
+
+app.post("/sign-up", (req, res) => {
+    const usuario = req.body;
+
+    usuarios.push(usuario);
+    res.send("OK");
+})
+
+app.post("/tweets", (req, res) => {
+    const { username, tweet } = req.body;
+
+    const usuarioCadastrado = usuarios.find(user => user.username === username);
+    
+    if(!usuarioCadastrado){
+        return res.send("UNAUTHORIZED");
+    }
+
+    const novoTweet = { username, tweet };
+    tweets.push(novoTweet);
+    return res.send("OK");
+})
+
+app.get("/tweets", (req, res) => {
+    
+})
 
 const PORT = 5000;
-app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`))
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
