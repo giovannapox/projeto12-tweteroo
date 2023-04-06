1d6ce0abb7dc6a0e0b91b9cabb1741305dda01a9
Giovanna <giovannad1clonius@gmail.com>
2023-04-05 23:30:03 -0300

    Arrumando post

--- src/app.js
+++ src/app.js
@@ -19,19 +19,20 @@ app.post("/tweets", (req, res) => {
     const { username, tweet } = req.body;
 
     const usuarioCadastrado = usuarios.find(user => user.username === username);
-    const avatar = usuarioCadastrado.avatar;
+    console.log(usuarioCadastrado)
     
-    if(usuarioCadastrado === undefined){
+    if(!usuarioCadastrado){
         return res.status(401).send("UNAUTHORIZED");
     }
 
-    const novoTweet = { username, tweet, avatar };
+    const novoTweet = { username, tweet };
     tweets.push(novoTweet);
     return res.status(201).send("OK");
 })
 
 app.get("/tweets", (req, res) => {
     const ultimosTweets = tweets.slice(-10);
+    
     return res.send(ultimosTweets);
 })
 
