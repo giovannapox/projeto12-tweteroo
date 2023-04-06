85893b338ea4c29a9abdb1a99dd10bd3a1f77696
Giovanna <giovannad1clonius@gmail.com>
2023-04-05 22:29:28 -0300

    Avatar inserido

--- src/app.js
+++ src/app.js
@@ -19,29 +19,22 @@ app.post("/tweets", (req, res) => {
     const { username, tweet } = req.body;
 
     const usuarioCadastrado = usuarios.find(user => user.username === username);
+    const avatar = usuarioCadastrado.avatar;
     
     if(!usuarioCadastrado){
         return res.status(401).send("UNAUTHORIZED");
     }
 
-    const novoTweet = { username, tweet };
+    const novoTweet = { username, tweet, avatar };
     tweets.push(novoTweet);
     return res.status(201).send("OK");
 })
 
 app.get("/tweets", (req, res) => {
     const ultimosTweets = tweets.slice(-10);
-    const objetoComAvatar = ultimosTweets.map(tweet => {
-        return {...tweet, avatar: procurarAvatar()}
-    })
-
-    return res.send(objetoComAvatar);
+    return res.send(ultimosTweets);
 })
 
-function procurarAvatar () {
-    const avatarUsuario = usuarios.find((u) => username === u.username);
-    return avatarUsuario.avatar;
-}
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
